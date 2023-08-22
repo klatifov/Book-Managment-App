f5108561824ea61515b3f46c568503124eeb8375
klatifov <126696244+klatifov@users.noreply.github.com>
2023-08-22 09:39:22 -0700

    Add files via upload

--- Book Managment App/assets/script.js
+++ Book Managment App/assets/script.js
@@ -1,17 +1,46 @@
 let kitablar = []
 
-let createInputForm = document.getElementById(inputForm)
-createForm.addEventListener("submit", function (event) {
-    event.preventDefault()});
+let kitabinAdi = document.getElementById('bookName')
+let kitabinMuellifi = document.getElementById("author")
+let kitabinIli = document.getElementById("pubDate")
 
-let nameInp = document.getElementById(bookName)
-let authorInp = document.getElementById(author)
-let pubDateInp = document.getElementById(pubDate)
+let form = document.getElementById("inputForm")
+form.addEventListener('submit', function(event){
+    event.preventDefault()
 
-let bookInfo = {
-name: nameInp.value,
-author: authorInp.value,
-pubDate: pubDateInp.value,
-}
+    let kitab = {
+        name: kitabinAdi.value,
+        author: kitabinMuellifi.value,
+        year: kitabinIli.value
+    }
 
-kitablar.push(bookInfo)
+    kitablar.push(kitab)
+
+    let tableBody = document.getElementById("tbody")
+    tableBody.innerHTML +=  `
+     <tr>
+        <td>${kitab.name}</td>
+        <td>${kitab.author}</td>
+        <td>${kitab.year}</td>
+        <td> <button class="delete">delete</button> </td>
+     </tr>
+     
+    ` 
+    let deleteBtns = document.querySelectorAll('.delete')
+
+    deleteBtns.forEach(function(deleteBtn){
+        deleteBtn.addEventListener('click', function() {
+            // tableBody.innerHTML = 
+            deleteBtn.parentElement.parentElement.remove()
+        })
+        
+    })
+    
+   
+
+})
+
+
+
+
+
